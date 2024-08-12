c25e41dec7ccbce34115ab74fb1ab4c7e5627bf2
HuseynAlizade <huseynalizade2010@gmail.com>
2024-08-13 00:02:27 +0400

    jshw3

---
+++ script.js
@@ -0,0 +1,44 @@
+//!Task1
+//var btn = document.getElementById("btn");
+//var text = document.getElementById("text");
+
+ //var userData = [];
+
+ //var userName = prompt("Please enter your username");
+//var userPassword = prompt("Please enter your password");
+//var userAge = +prompt("Please enter your age");
+
+//function register() {
+  //if (userAge < 18) {
+    //alert(userName + " You must be at least 18 years old to register.");
+  //} else {
+   // userData.push(userName, userPassword);
+   // text.innerText = userName + "is now registered";
+ // }
+
+  //console.log(userData);
+//}
+
+//todo Task2
+
+//var kids = [];
+//console.log(kids);
+
+//kids.unshift("Faiq", "Metin", "Yusif", "Resul", "Eli", "Huseyn");
+//console.log(kids);
+
+//kids.shift();
+//console.log(kids);
+
+//kids.unshift("Murad");
+//console.log(kids);
+
+//kids.push("Jhon");
+//console.log(kids);
+
+//kids.pop();
+//console.log(kids);
+
+//kids.shift();
+//console.log(kids);
+
